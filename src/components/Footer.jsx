639534a20da241e6b936795ddc143ba6edf17ac9
moamel97 <muamal1997@gmail.com>
2024-07-05 02:03:29 -0700

    adding a footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import {Link} from 'react-router-dom';
+import './Footer.css';
+
+
+export default function Footer() {
+  return(
+    <footer>
+    </footer>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import HomeMain from '../components/HomeMain';
+import Footer from '../components/footer';
 export default function Home() {
   return(
     <>
     <Navbar></Navbar>
     <HomeMain></HomeMain>
+    <Footer></Footer>
     </>
   );
 }
