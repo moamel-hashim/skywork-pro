a6c8ce55e0f02f313e5f1ca99f07a01b44baa95f
moamel97 <muamal1997@gmail.com>
2024-07-05 23:31:08 -0700

    editing footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
-
+import Service from './pages/Service';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/service" element={<Service/>}/>
     </Routes>
   )
 }
---
+++ src/pages/Service.jsx
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function Service() {
+  return(
+    <>
+    <Navbar></Navbar>
+    <Footer></Footer>
+    </>
+  )
+}
