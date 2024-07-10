074837d728befdce79048792a4290f69a07afc33
moamel97 <muamal1997@gmail.com>
2024-07-10 14:52:46 -0700

    adding service page

---
+++ src/components/ServiceMain.jsx
@@ -0,0 +1,48 @@
+
+import './ServiceMain.css';
+export default function ServiceMain() {
+  return (
+    <main>
+      <header>
+        <div>
+          <h2>What We Offer</h2>
+        </div>
+        <div>
+          <p>
+            At Skywork pro we strive to improve the performance and lifespan of your utility assets by utilizing insightful data. To ensure optimal conditions and uninterrupted operations, Skywork pro provides a range of specialized services that emphasize proactive analytics and responsive insights to help utilities manage and enhance asset performance.
+            Our cutting-edge tools and technology enable precise and safe inspections of infrastructure conditions, allowing for timely maintenance and early detection of potential risks. By combining intelligence-driven technology with our team's expertise, Aethon delivers valuable, cost-effective, and innovative solutions to our clients.
+          </p>
+        </div>
+      </header>
+      <section>
+        <div>
+          <h3>Services</h3>
+          <p>Here at Skywork pro we offer a wide verity of services such as inspections, LiDAR technology, Mapping
+            orthomosaics, 3D Models, and infrared/RGB/Thermal
+          </p>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h3>Imaging</h3>
+          Skywork pro offers disaster relief, wildfire Mitigation, PSPS services, security, and project analysis
+        </div>
+      </section>
+      <section>
+        <div>
+          <h3>Management</h3>
+          <p>We also offer an excelnt service in vegetation management, magnetometer, surveys and hydrology & water shed</p>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h3>Erosion</h3>
+          <p>Skywork pro offers service in phtography & cinematography</p>
+        </div>
+      </section>
+      <section>
+        <p>Dont see a service you need above? Contact us to have Skywork pro tailor a solution that fits your needs.</p>
+      </section>
+    </main>
+  )
+}
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,10 +1,11 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-
+import ServiceMain from "../components/ServiceMain";
 export default function Service() {
   return(
     <>
     <Navbar></Navbar>
+    <ServiceMain></ServiceMain>
     <Footer></Footer>
     </>
   )
