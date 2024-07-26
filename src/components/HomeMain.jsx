8572a008b658ba39812c9949e338db89989fb7eb
moamel97 <muamal1997@gmail.com>
2024-07-25 20:19:28 -0700

    changed text in the hero banner

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -10,9 +10,10 @@ export default function HomeMain() {
         <div className="hero-img-container">
           <div className="hero-text">
             <h3>Transmission & Distribution Inspections</h3>
-            <p>Skywork Pro is a leading provider of advance inspection service for transmission and distribution networks.
-              Our mission is to ensure the safety, security, and integrity of critical infrastructure, protecting and enhancing
-              the safety and security of our communities.
+            <p>
+              Skywork Pro is a premier UAS (unmanned aerial systems) comprehensive service provider for utility networks and public safety.
+               Our team is made of seasoned veterans specialized in the use of UAV's (unmanned aerial vehicles).
+               Our mission is to ensure the safety, security, and integrity of critical infrastructure and the public, enhancing the safety and security of our communities.
             </p>
             <div className="button-container">
               <Link to='/contact'>Contact Us</Link>
